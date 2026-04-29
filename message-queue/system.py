7b8a9e3349087b68ac3f076890b589fda026d1d2
hirochika-f <hirochika.fujiki@gmail.com>
2026-04-30 00:39:23 +0900

    add: explicit topic lifecycle

--- message-queue/system.py
+++ message-queue/system.py
@@ -5,21 +5,36 @@ import threading
 
 class MessageQueue:
     def __init__(self):
+        self.messages = {}
+        self.offsets = {}
+        self.subscribers = {}
+
+    def create_topic(self, topic: str) -> bool:
+        if topic in self.messages:
+            raise ValueError(f"Already exists: {topic}")
+
         # self.messages = {topic: [message1, message2, ...]}
-        self.messages = defaultdict(list)
-        # self.offsets = {consumer: {topic: offset}}
-        self.offsets = defaultdict(lambda: defaultdict(int))
+        self.messages[topic] = []
         # self.subscribers = {topic: {consumer1, consumer2, ...}}
-        self.subscribers = defaultdict(set)
+        self.subscribers[topic] = set()
+        return True
 
     def subscribe(self, topic: str, consumer_id: str) -> bool:
+        if topic not in self.messages:
+            raise ValueError(f"Not found topic: {topic}")
         if consumer_id in self.subscribers[topic]:
             return False
+
         self.subscribers[topic].add(consumer_id)
+        if consumer_id not in self.offsets:
+            self.offsets[consumer_id] = {}
         self.offsets[consumer_id][topic] = len(self.messages[topic])
         return True
 
     def publish(self, topic: str, message: str) -> int:
+        if topic not in self.messages:
+            raise ValueError(f"Not found topic: {topic}")
+
         self.messages[topic].append(message)
         return len(self.subscribers[topic])
 
@@ -28,6 +43,7 @@ class MessageQueue:
             raise ValueError(f"Consumer: {consumer_id} subscribes no topics.")
         if topic not in self.offsets[consumer_id]:
             raise ValueError(f"Not found topic: {topic}")
+
         offset = self.offsets[consumer_id][topic]
         if offset >= len(self.messages[topic]):
             return ""
@@ -38,6 +54,7 @@ class MessageQueue:
 
 if __name__ == "__main__":
     mq = MessageQueue()
+    mq.create_topic("orders")
     mq.subscribe("orders", "consumer1")
     mq.subscribe("orders", "consumer2")
 
